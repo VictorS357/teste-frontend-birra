2f5519778836d084b77ce4a56ee302f4eb78a648
VictorS357 <victorfransousa357@gmail.com>
2026-07-29 16:14:49 -0300

    feat: adiciona model MovMassaFilho

---
+++ src/models/MovMassaFilho.js
@@ -0,0 +1,39 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    const MovMassaFilho = sequelize.define(
+        'MovMassaFilho',
+        {
+            id: {
+                type: DataTypes.UUID,
+                primaryKey: true,
+                allowNull: false,
+                field: 'id'
+            },
+
+            movMassaPaiId: {
+                type: DataTypes.UUID,
+                allowNull: false,
+                field: 'mov_massa_pai_id'
+            },
+
+            equipRecipId: {
+                type: DataTypes.UUID,
+                allowNull: false,
+                field: 'equip_recip_id'
+            },
+
+            timestamp: {
+                type: DataTypes.DATE,
+                allowNull: true,
+                field: 'timestamp'
+            }
+        },
+        {
+            tableName: 'mov_massa_filho',
+            timestamps: false
+        }
+    );
+
+    return MovMassaFilho;
+};
