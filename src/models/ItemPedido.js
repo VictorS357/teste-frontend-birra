570e11343a6422f5ace271a64ddf3b7fb5092b98
VictorS357 <victorfransousa357@gmail.com>
2026-07-29 15:22:50 -0300

    feat: adiciona model ItemPedido

---
+++ src/models/ItemPedido.js
@@ -0,0 +1,111 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    const ItemPedido = sequelize.define(
+        'ItemPedido',
+        {
+            id: {
+                type: DataTypes.UUID,
+                primaryKey: true,
+                allowNull: false,
+                field: 'id'
+            },
+
+            identificador: {
+                type: DataTypes.STRING,
+                allowNull: true,
+                field: 'identificador'
+            },
+
+            pedidoId: {
+                type: DataTypes.UUID,
+                allowNull: false,
+                field: 'pedido_id'
+            },
+
+            produtoId: {
+                type: DataTypes.UUID,
+                allowNull: false,
+                field: 'produto_id'
+            },
+
+            qtde: {
+                type: DataTypes.DECIMAL(10, 2),
+                allowNull: false,
+                field: 'qtde'
+            },
+
+            valorUnit: {
+                type: DataTypes.DECIMAL(10, 2),
+                allowNull: false,
+                field: 'valor_unit'
+            },
+
+            ajuste: {
+                type: DataTypes.DECIMAL(5, 2),
+                allowNull: true,
+                field: 'ajuste'
+            },
+
+            ajuste2: {
+                type: DataTypes.STRING,
+                allowNull: true,
+                field: 'ajuste2'
+            },
+
+            valorTotal: {
+                type: DataTypes.DECIMAL(10, 2),
+                allowNull: true,
+                field: 'valor_total'
+            },
+
+            obs: {
+                type: DataTypes.TEXT,
+                allowNull: true,
+                field: 'obs'
+            },
+
+            bonificacao: {
+                type: DataTypes.BOOLEAN,
+                allowNull: true,
+                field: 'bonificacao'
+            },
+
+            volumes: {
+                type: DataTypes.INTEGER,
+                allowNull: true,
+                field: 'volumes'
+            },
+
+            liberado: {
+                type: DataTypes.BOOLEAN,
+                allowNull: true,
+                field: 'liberado'
+            },
+
+            sobra: {
+                type: DataTypes.DECIMAL(10, 2),
+                allowNull: true,
+                field: 'sobra'
+            },
+
+            entregue: {
+                type: DataTypes.BOOLEAN,
+                allowNull: true,
+                field: 'entregue'
+            },
+
+            concluido: {
+                type: DataTypes.BOOLEAN,
+                allowNull: true,
+                field: 'concluido'
+            }
+        },
+        {
+            tableName: 'itens_pedido',
+            timestamps: false
+        }
+    );
+
+    return ItemPedido;
+};
