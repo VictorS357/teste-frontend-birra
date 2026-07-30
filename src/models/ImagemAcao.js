2fced5f2e2fb6a6a66d2ae5fd0ab0bfa10ba793c
VictorS357 <victorfransousa357@gmail.com>
2026-07-30 14:27:45 -0300

    feat: adiciona model ImagemAcao

---
+++ src/models/ImagemAcao.js
@@ -0,0 +1,51 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    const ImagemAcao = sequelize.define(
+        'ImagemAcao',
+        {
+            id: {
+                type: DataTypes.UUID,
+                primaryKey: true,
+                allowNull: false,
+                field: 'id'
+            },
+
+            descricao: {
+                type: DataTypes.STRING,
+                allowNull: true,
+                field: 'descricao'
+            },
+
+            acaoId: {
+                type: DataTypes.UUID,
+                allowNull: true,
+                field: 'acao_id'
+            },
+
+            img: {
+                type: DataTypes.STRING,
+                allowNull: true,
+                field: 'time'
+            },
+
+            responsavelId: {
+                type: DataTypes.UUID,
+                allowNull: true,
+                field: 'responsavel_id'
+            },
+
+            dataEHora: {
+                type: DataTypes.DATE,
+                allowNull: true,
+                field: 'data_e_hora'
+            }
+        },
+        {
+            tableName: 'imagens_acoes',
+            timestamps: false
+        }
+    );
+
+    return ImagemAcao;
+};
