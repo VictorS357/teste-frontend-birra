f6b745a4de408fbe2e8015862b3dae66a4ded964
VictorS357 <victorfransousa357@gmail.com>
2026-07-30 13:33:18 -0300

    feat: adiciona o model planejamento

---
+++ src/models/Planejamento.js
@@ -0,0 +1,117 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    const Planejamento = sequelize.define(
+        'Planejamento',
+        {
+            id: {
+                type: DataTypes.UUID,
+                primaryKey: true,
+                allowNull: false,
+                field: 'id'
+            },
+
+            dataSolicitacao: {
+                type: DataTypes.DATEONLY,
+                allowNull: true,
+                field: 'data_solicitacao'
+            },
+
+            descricao: {
+                type: DataTypes.TEXT,
+                allowNull: true,
+                field: 'descricao'
+            },
+
+            tipo: {
+                type: DataTypes.STRING,
+                allowNull: true,
+                field: 'tipo'
+            },
+
+            status: {
+                type: DataTypes.STRING,
+                allowNull: true,
+                field: 'status'
+            },
+
+            responsavelId: {
+                type: DataTypes.UUID,
+                allowNull: true,
+                field: 'responsavel_id'
+            },
+
+            urgencia: {
+                type: DataTypes.STRING,
+                allowNull: true,
+                field: 'urgencia'
+            },
+
+            parecerDoResponsavel: {
+                type: DataTypes.STRING,
+                allowNull: true,
+                field: 'parecer_do_responsavel'
+            },
+
+            prazoDeConclusao: {
+                type: DataTypes.DATEONLY,
+                allowNull: true,
+                field: 'prazo_de_conclusao'
+            },
+
+            clienteId: {
+                type: DataTypes.UUID,
+                allowNull: true,
+                field: 'cliente_id'
+            },
+
+            parecerDoCliente: {
+                type: DataTypes.STRING,
+                allowNull: true,
+                field: 'parecer_do_cliente'
+            },
+
+            assinaturaDoCliente: {
+                type: DataTypes.STRING,
+                allowNull: true,
+                field: 'assinatura_do_cliente'
+            },
+
+            dataDeConclusao: {
+                type: DataTypes.DATEONLY,
+                allowNull: true,
+                field: 'data_de_conclusao'
+            },
+
+            solicitanteId: {
+                type: DataTypes.UUID,
+                allowNull: true,
+                field: 'solicitante_id'
+            },
+
+            dataInicio: {
+                type: DataTypes.DATEONLY,
+                allowNull: true,
+                field: 'data_inicio'
+            },
+
+            privacidade: {
+                type: DataTypes.STRING,
+                allowNull: true,
+                field: 'privacidade'
+            },
+
+            foto: {
+                type: DataTypes.STRING,
+                allowNull: true,
+                field: 'foto'
+            }
+        },
+        {
+            tableName: 'planejamento',
+            timestamps: false
+        }
+    );
+
+    return Planejamento;
+};
