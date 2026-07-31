11475250c51b21e8e8dbbb38b2cd54994ee5768c
VictorS357 <victorfransousa357@gmail.com>
2026-07-31 16:16:29 -0300

    feat: adiciona migration da tabela comprovantes

---
+++ migrations/20260731191123-create-comprovantes.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('comprovantes', {
+      id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        primaryKey: true
+      },
+
+      path: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+
+      file: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+
+      create_time: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+
+      last_modified_by: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+
+      mime_type: {
+        type: Sequelize.STRING,
+        allowNull: true
+      }
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('comprovantes');
+  }
+};
