48c21a73e59b44de722cb02ae3dd881ed00713c5
VictorS357 <victorfransousa357@gmail.com>
2026-08-13 15:11:09 -0300

    fix(frontend): arruma bug de não exibição da sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,27 +1,29 @@
 import { NavLink } from "react-router";
 
 function Sidebar() {
-    <aside className="sidebar">
-        <h1>Banco Migrado</h1>
+    return (
+        <aside className="sidebar">
+            <h1>Banco Migrado</h1>
 
-        <nav>
-            <NavLink to="/">
-                Dashboard
-            </NavLink>
+            <nav>
+                <NavLink to="/">
+                    Dashboard
+                </NavLink>
 
-            <NavLink to="/clientes">
-                Clientes
-            </NavLink>
+                <NavLink to="/clientes">
+                    Clientes
+                </NavLink>
 
-            <NavLink to="/produtos">
-                Produtos
-            </NavLink>
+                <NavLink to="/produtos">
+                    Produtos
+                </NavLink>
 
-            <NavLink to="/pedidos">
-                Pedidos
-            </NavLink>
-        </nav>
-    </aside>
+                <NavLink to="/pedidos">
+                    Pedidos
+                </NavLink>
+            </nav>
+        </aside>
+    );
 }
 
 export default Sidebar;
