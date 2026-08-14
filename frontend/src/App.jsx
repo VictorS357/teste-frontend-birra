4739b7ca8344ace19cc79626e8ce104e9bfdd6ad
VictorS357 <victorfransousa357@gmail.com>
2026-08-14 14:39:36 -0300

    feat(frontend): adiciona rotas para todos os arquivos feitos anteriormente

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,17 @@ import Dashboard from './pages/Dashboard';
 import Clientes from './pages/Clientes';
 import Produtos from './pages/Produtos';
 import Pedidos from './pages/Pedidos';
+import Comprovantes from "./pages/Comprovantes";
+import EquipCliente from "./pages/EquipCliente";
+import EquipRecip from "./pages/EquipRecip";
+import Etiquetas from "./pages/Etiquetas";
+import Files from "./pages/Files";
+import HistoricoMovimentacoes from "./pages/HistoricoMovimentacoes";
+import ImagensAcoes from "./pages/ImagensAcoes";
+import ItensPedido from "./pages/ItensPedido";
+import Planejamento from "./pages/Planejamento";
+import TabelaPreco from "./pages/TabelaPreco";
+import Usuarios from "./pages/Usuarios";
 
 function App() {
   return (
@@ -14,6 +25,17 @@ function App() {
         <Route path="/clientes" element={<Clientes />} />
         <Route path="/produtos" element={<Produtos />} />
         <Route path="/pedidos" element={<Pedidos />} />
+        <Route path="/comprovantes" element={<Comprovantes />} />
+        <Route path="/equip-cliente" element={<EquipCliente />} />
+        <Route path="/equip-recip" element={<EquipRecip />} />
+        <Route path="/etiquetas" element={<Etiquetas />} />
+        <Route path="/files" element={<Files />} />
+        <Route path="/historico-movimentacoes" element={<HistoricoMovimentacoes />} />
+        <Route path="/imagens-acoes" element={<ImagensAcoes />} />
+        <Route path="/itens-pedido" element={<ItensPedido />} />
+        <Route path="/planejamento" element={<Planejamento />} />
+        <Route path="/tabela-preco" element={<TabelaPreco />} />
+        <Route path="/usuarios" element={<Usuarios />} />
       </Route>
     </Routes>
   );
